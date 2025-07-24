d0a07306d8cbed85d9853791d841117628e34abd
Laktionov <ber_lak@mail.ru>
2025-07-24 13:30:43 +0500

    Category and Product upgrated

--- src/category.py
+++ src/category.py
@@ -1,12 +1,28 @@
+from src.product import Product
+
+
 class Category:
     name:str
     description:str
     products:list
     category_count = 0
     product_count = 0
+
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+        return product_str
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
--- src/product.py
+++ src/product.py
@@ -3,8 +3,26 @@ class Product:
     description:str
     price:float
     quantity:int
+
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @classmethod
+    def new_product(cls, product):
+        name = product['name']
+        description = product['description']
+        price = product['price']
+        quantity = product['quantity']
+        return cls(name, description, price, quantity)
+
+    @price.setter
+    def price(self, price):
+        self.__price = price if price > 0 else "Цена не должна быть нулевая или отрицательная"
+
