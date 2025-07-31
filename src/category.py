8f6943843a2006ccb7d1b5caee68822904c642f9
Laktionov <ber_lak@mail.ru>
2025-07-31 11:11:55 +0500

    category updated

--- src/category.py
+++ src/category.py
@@ -16,9 +16,16 @@ class Category:
         Category.product_count += len(products) if products else 0
 
     def __str__(self):
-        return f'{self.name}, количество продуктов: {Category.product_count} шт.'
+        product_list = self.__products
+        product_quantity = []
+        for product in product_list:
+            product_dict = product.__dict__
+            product_quantity.append(product_dict['quantity'])
+
+        return f'{self.name}, количество продуктов: {sum(product_quantity)} шт.'
 
     @property
+
     def products(self):
         product_str = ""
         for product in self.__products:
